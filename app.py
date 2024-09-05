d82b67bec4e83cbd30e36ea15ce3a7b25ba9414b
prashant-singh <prashant.singh@increff.com>
2024-09-05 10:44:53 +0530

    updated venv

---
+++ app.py
@@ -0,0 +1,30 @@
+from templates import create_app
+from flask import make_response
+
+from azure.identity import DefaultAzureCredential
+
+from azure.mgmt.datafactory import DataFactoryManagementClient
+from flask_cors import CORS
+
+from flask_login import login_user, logout_user,LoginManager,UserMixin,login_required,current_user
+from flask import session
+
+app=create_app()
+CORS(app,supports_credentials=True)
+
+def main():
+
+    @app.route('/')
+    def index():
+        response = make_response('Hello, World!')
+        response.headers['Access-Control-Allow-Origin']='*'
+        return response
+
+    app.run(debug=True)
+
+
+
+
+if __name__=='__main__':
+    main()
+    
--- templates/views.py
+++ templates/views.py
@@ -120,73 +120,202 @@ def createRun(factory, pipeline):
         logging.error(traceback.format_exc())
         return jsonify({"error": str(e)}), 500
 
-@views.route('/cancelRun/<factory>/<pipeline_name>', methods=['POST'])
-def cancelRun(factory, pipeline_name):
-    try:
-        # Initialize Data Factory Management Client
-        client = DataFactoryManagementClient(
-            credential=DefaultAzureCredential(),
-            subscription_id=os.getenv("AZURE_SUBSCRIPTION_ID"),
-        )
+# @views.route('/cancelRun/<factory>/<pipeline_name>', methods=['POST'])
+# def cancelRun(factory, pipeline_name):
+#     try:
+#         # Initialize Data Factory Management Client
+#         client = DataFactoryManagementClient(
+#             credential=DefaultAzureCredential(),
+#             subscription_id=os.getenv("AZURE_SUBSCRIPTION_ID"),
+#         )
 
-        # Define the time window to search for recent runs (e.g., last 24 hours)
-        end_time = datetime.utcnow()
-        start_time = end_time - timedelta(days=7)
+#         # Define the time window to search for recent runs (e.g., last 24 hours)
+#         end_time = datetime.utcnow()
+#         start_time = end_time - timedelta(days=7)
 
-        # Get the latest pipeline run
-        pipeline_runs = client.pipeline_runs.query_by_factory(
-            resource_group_name="rg-ms-etl-prod",
-            factory_name=factory,
-            filter_parameters={
-                "lastUpdatedAfter": start_time,
-                "lastUpdatedBefore": end_time,
-                "filters": [
-                    {"operand": "PipelineName", "operator": "Equals", "values": [pipeline_name]}
-                ]
-            }
-        )
+#         # Get the latest pipeline run
+#         pipeline_runs = client.pipeline_runs.query_by_factory(
+#             resource_group_name="rg-ms-etl-prod",
+#             factory_name=factory,
+#             filter_parameters={
+#                 "lastUpdatedAfter": start_time,
+#                 "lastUpdatedBefore": end_time,
+#                 "filters": [
+#                     {"operand": "PipelineName", "operator": "Equals", "values": [pipeline_name]}
+#                 ]
+#             }
+#         )
 
-        # Check if there are any pipeline runs
-        if not pipeline_runs.value:
-            return jsonify({"error": f"No runs found for pipeline {pipeline_name} in the last 24 hours"}), 404
+#         # Check if there are any pipeline runs
+#         if not pipeline_runs.value:
+#             return jsonify({"error": f"No runs found for pipeline {pipeline_name} in the last 24 hours"}), 404
 
-        # Assuming the latest run is the first one (sorted by LastUpdated)
-        latest_run = max(pipeline_runs.value, key=lambda run: run.last_updated)
-        latest_run_id = latest_run.run_id
+#         # Assuming the latest run is the first one (sorted by LastUpdated)
+#         latest_run = max(pipeline_runs.value, key=lambda run: run.last_updated)
+#         latest_run_id = latest_run.run_id
 
-        # Cancel the latest run
+#         # Cancel the latest run
+#         client.pipeline_runs.cancel(
+#             resource_group_name="rg-ms-etl-prod",
+#             factory_name=factory,
+#             run_id=latest_run_id,
+#         )
+
+#         #cancel the child pipeliens
+#         child_runs = client.pipeline_runs.query_by_factory(
+#             resource_group_name="rg-ms-etl-prod",
+#             factory_name=factory,
+#             filter_parameters={
+#                 "lastUpdatedAfter": start_time,
+#                 "lastUpdatedBefore": end_time,
+#                 "filters": [
+#                     {"operand": "TriggeredByName", "operator": "Equals", "values": [pipeline_name]}
+#                 ]
+#             }
+#         )
+
+#         # Cancel all identified child runs
+#         for run in child_runs.value:
+#             client.pipeline_runs.cancel(
+#                 resource_group_name="rg-ms-etl-prod",
+#                 factory_name=factory,
+#                 run_id=run.run_id,
+#             )
+
+
+#         return jsonify({"message": f"Run {latest_run_id} for pipeline {pipeline_name} cancelled successfully"}), 200
+
+#     except Exception as e:
+#         logging.error(f"An error occurred: {str(e)}")
+#         logging.error(traceback.format_exc())
+#         return jsonify({"error": str(e)}), 500
+
+
+@views.route('/cancelRun/<data_factory>/<pipeline_name>', methods=['POST'])
+def CancelRecursively(data_factory,pipeline_name):
+    latest_run = get_recent_pipeline_run_id(data_factory,pipeline_name)
+    latest_run_dict = get_parentpipeline_dict(latest_run)
+    if latest_run_dict['status']=='Success':
+         return jsonify({"error": f"Pipeline {pipeline_name} is Successful So the run cannot be cancelled"}), 404
+    elif latest_run_dict['status']=='Cancelled':
+        return jsonify({"error": f"Pipeline {pipeline_name}  is already Cancelled'"}), 404
+    else:
+        cancelPipeline(latest_run_dict['run_id'],data_factory)
+        pipelines_to_cancel = traverse_responses_recursively(latest_run_dict['run_id'],data_factory,latest_run_dict['time_parameters'])
+        print(pipelines_to_cancel)
+        return jsonify({"message": f"Run {latest_run_dict['run_id']} for pipeline {pipeline_name} cancelled successfully"}), 200                   
+    return jsonify({"error": f"Run Ended'"}), 404     
+
+
+
+def get_recent_pipeline_run_id(factory,pipeline_name):
+    client = DataFactoryManagementClient(
+    credential=DefaultAzureCredential(),        
+    subscription_id=os.getenv("AZURE_SUBSCRIPTION_ID"))
+        
+    end_time = datetime.utcnow()
+    start_time = end_time - timedelta(days=7)
+    pipeline_runs = client.pipeline_runs.query_by_factory(
+        resource_group_name="rg-ms-etl-prod",
+        factory_name=factory,
+        filter_parameters={
+            "lastUpdatedAfter": start_time,
+            "lastUpdatedBefore": end_time,
+            "filters": [
+                {"operand": "PipelineName", "operator": "Equals", "values": [pipeline_name]}
+            ]
+        }
+    )
+
+    if not pipeline_runs.value:
+        return ({"error": f"No runs found for pipeline {pipeline_name} in the last 24 hours"}), 404
+
+    latest_run = max(pipeline_runs.value, key=lambda run: run.last_updated)
+    return latest_run
+
+
+def converttime(timestr):
+    time_obj = datetime.fromisoformat(timestr)
+    iso_time_str = time_obj.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-3] + 'Z'
+    return iso_time_str
+
+def add_hour_to_isotime(timestamp):
+    dt = datetime.fromisoformat(timestamp.replace('Z', '+00:00'))
+    new_dt = dt + timedelta(hours=1)
+    new_timestamp = new_dt.isoformat().replace('+00:00', 'Z') 
+    return new_timestamp
+
+
+def get_pipeline_responses(run_id,time_params,data_factory):
+    client = DataFactoryManagementClient(
+        credential=DefaultAzureCredential(),        
+        subscription_id=os.getenv("AZURE_SUBSCRIPTION_ID"))
+    
+    response = client.activity_runs.query_by_pipeline_run(
+        resource_group_name="rg-ms-etl-prod",
+        factory_name=data_factory,
+        run_id=run_id,
+        filter_parameters={
+            "lastUpdatedAfter": time_params['start_time'],
+            "lastUpdatedBefore": time_params['end_time'],
+        },
+    )
+    return response
+
+
+def get_parentpipeline_dict(latest_run):
+    latest_run_dict = {
+        'run_id'   : latest_run.run_id,
+        'run_group_id' : latest_run.run_group_id,
+        'is_Latest' : latest_run.is_latest,
+        'pipeline_name' : latest_run.pipeline_name,
+        'status': latest_run.status,
+        'time_parameters' : {
+        'start_time' : converttime(str(latest_run.run_start)),
+        'end_time' : converttime(str(latest_run.last_updated))}
+    }
+    if latest_run_dict['status']=='InProgress':
+        latest_run_dict['time_parameters']['end_time'] = add_hour_to_isotime(latest_run_dict['time_parameters']['end_time'])
+
+    return latest_run_dict
+
+def get_childpipelines_response_data(child_pipelinerun):
+    dict_val = {
+        'parent_pipelineName': child_pipelinerun.pipeline_name,
+        'parent_pipeline_run_id' : child_pipelinerun.pipeline_run_id,
+        'activity_name' : child_pipelinerun.activity_name,
+        'activity_type' : child_pipelinerun.activity_type,
+        'status' : child_pipelinerun.status,
+        'pipelineName' :  None if child_pipelinerun.output is None or child_pipelinerun.output.get("pipelineName") is None else child_pipelinerun.output['pipelineName'],
+        'pipelineRunId': None if child_pipelinerun.output is None or child_pipelinerun.output.get("pipelineRunId") is None else child_pipelinerun.output['pipelineRunId']
+    }
+    return dict_val
+
+def traverse_responses_recursively(parent_run_id,data_factory,time_parameters,pipelines_to_cancel = None):
+    if pipelines_to_cancel is None:
+        pipelines_to_cancel = []
+
+    child_activitys = [get_childpipelines_response_data(val) for val in get_pipeline_responses(parent_run_id,time_parameters,data_factory).value if (val.activity_type=='ExecutePipeline')]
+    for child_activity in child_activitys:
+        
+        if child_activity['pipelineRunId']:
+            if child_activity['activity_type'] =='ExecutePipeline':
+                cancelPipeline(child_activity['pipelineRunId'],data_factory)
+                pipelines_to_cancel.append(child_activity)
+                traverse_responses_recursively(child_activity['pipelineRunId'],data_factory,time_parameters,pipelines_to_cancel)
+                
+    return pipelines_to_cancel
+
+def cancelPipeline(run_id,data_factory):
+    client = DataFactoryManagementClient(
+    credential=DefaultAzureCredential(),        
+    subscription_id=os.getenv("AZURE_SUBSCRIPTION_ID"))
+    try: 
         client.pipeline_runs.cancel(
-            resource_group_name="rg-ms-etl-prod",
-            factory_name=factory,
-            run_id=latest_run_id,
-        )
-
-        #cancel the child pipeliens
-        child_runs = client.pipeline_runs.query_by_factory(
-            resource_group_name="rg-ms-etl-prod",
-            factory_name=factory,
-            filter_parameters={
-                "lastUpdatedAfter": start_time,
-                "lastUpdatedBefore": end_time,
-                "filters": [
-                    {"operand": "TriggeredByName", "operator": "Equals", "values": [pipeline_name]}
-                ]
-            }
-        )
-
-        # Cancel all identified child runs
-        for run in child_runs.value:
-            client.pipeline_runs.cancel(
-                resource_group_name="rg-ms-etl-prod",
-                factory_name=factory,
-                run_id=run.run_id,
-            )
-
-
-        return jsonify({"message": f"Run {latest_run_id} for pipeline {pipeline_name} cancelled successfully"}), 200
-
-    except Exception as e:
-        logging.error(f"An error occurred: {str(e)}")
-        logging.error(traceback.format_exc())
-        return jsonify({"error": str(e)}), 500
+                            resource_group_name="rg-ms-etl-prod",
+                            factory_name=data_factory,
+                            run_id=run_id,
+                        )
+    except HttpResponseError as ex:
+        print (f"run id : {run_id} run is Completed or Cancelled Already Exception:{ex}")
 
